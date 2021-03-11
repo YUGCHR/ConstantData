7db9fb0160b4df06888e028d01aa268291a28de6
Yuriy <47811171+YUGCHR@users.noreply.github.com>
2021-03-11 15:45:29 +0000

    TaskDescriptionAndProgress was added

---
+++ Shared.Library/Models/TaskDescriptionAndProgress.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Shared.Library.Models
+{
+    public class TaskDescriptionAndProgress
+    {
+        [JsonProperty(PropertyName = "tasksCount")]
+
+        public int TasksCount { get; set; }
+
+        public class TaskComplicatedDescription
+        {
+            [JsonProperty(PropertyName = "cycleCount")]
+            public int CycleCount { get; set; }
+        }
+
+        public class TaskProgressState
+        {
+            [JsonProperty(PropertyName = "isTaskRunning")]
+            public bool IsTaskRunning { get; set; }
+        }
+    }
+}
