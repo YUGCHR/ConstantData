604c675cd053aca5c76d1aed9eb65f877817765d
Yuriy <47811171+YUGCHR@users.noreply.github.com>
2021-04-16 23:36:15 +0100

    Serilog was added to ConstantData (MonitorLoop)

--- ConstantData/MonitorLoop.cs
+++ ConstantData/MonitorLoop.cs
@@ -14,7 +14,7 @@ namespace ConstantData
 {
     public class MonitorLoop
     {
-        private readonly ILogger<MonitorLoop> _logger;
+        //private readonly ILogger<MonitorLoop> _logger;
         private readonly IInitConstantsService _init;
         private readonly ISharedDataAccess _data;
         private readonly ICacheManageService _cache;
@@ -23,7 +23,7 @@ namespace ConstantData
         private readonly string _guid;
 
         public MonitorLoop(
-            ILogger<MonitorLoop> logger,
+            //ILogger<MonitorLoop> logger,
             GenerateThisInstanceGuidService thisGuid,
             ISharedDataAccess data,
             ICacheManageService cache,
@@ -31,7 +31,7 @@ namespace ConstantData
             IHostApplicationLifetime applicationLifetime, 
             IInitConstantsService init)
         {
-            _logger = logger;
+            //_logger = logger;
             _data = data;
             _constantService = constantService;
             _init = init;
@@ -40,11 +40,15 @@ namespace ConstantData
             _guid = thisGuid.ThisBackServerGuid();
         }
 
+        private static Serilog.ILogger Logs => Serilog.Log.ForContext<MonitorLoop>();
+
         private const string CheckToken = "tt-tt-tt";
 
         public void StartMonitorLoop()
         {
-            _logger.LogInformation("ConstantsMountingMonitor Loop is starting.");
+            //_logger.LogInformation("ConstantsMountingMonitor Loop is starting.");
+            Logs.Here().Information("ConstantsMountingMonitor Loop is starting.");
+
 
             // Run a console user input loop in a background thread
             Task.Run(ConstantsMountingMonitor, _cancellationToken);
@@ -71,10 +75,12 @@ namespace ConstantData
         {
             EventKeyNames eventKeysSet = _init.InitialiseEventKeyNames();
 
-            _logger.LogInformation(10351, "1 ConstantCheck EventKeyFrontGivesTaskTimeDays = {0}.", eventKeysSet.EventKeyFrontGivesTaskTimeDays);
+            //_logger.LogInformation(10351, "1 ConstantCheck EventKeyFrontGivesTaskTimeDays = {0}.", eventKeysSet.EventKeyFrontGivesTaskTimeDays);
+            Logs.Here().Information("ConstantCheck EventKeyFrontGivesTaskTimeDays = {0}.", eventKeysSet.EventKeyFrontGivesTaskTimeDays);
 
             (string startConstantKey, string constantsStartLegacyField, string constantsStartGuidField) = _data.FetchBaseConstants();
-            _logger.LogInformation(10350, "ConstantData send constants {0} to SetStartConstants.", eventKeysSet, "constants");
+            //_logger.LogInformation(10350, "ConstantData send constants {0} to SetStartConstants.", eventKeysSet, "constants");
+            Logs.Here().Information("ConstantData send constants {0} to SetStartConstants.");
 
             // записываем константы в стартовый ключ и старое поле (для совместимости)
             await _cache.SetStartConstants(startConstantKey, constantsStartLegacyField, eventKeysSet);
@@ -99,7 +105,9 @@ namespace ConstantData
                 if (_cancellationToken.IsCancellationRequested)
                 {
                     bool res = await _cache.DeleteKeyIfCancelled(startConstantKey);
-                    _logger.LogInformation(310310, "_cancellationToken was received, key was removed = {KeyStroke}.", res);
+                    //_logger.LogInformation(310310, "_cancellationToken was received, key was removed = {KeyStroke}.", res);
+                    Logs.Here().Warning("Cancellation Token was received, key was removed = {KeyStroke}.", res);
+
                     return;
                 }
                 
@@ -107,7 +115,8 @@ namespace ConstantData
 
                 if (keyStroke.Key == ConsoleKey.W)
                 {
-                    _logger.LogInformation(10370, "ConsoleKey was received {KeyStroke}.", keyStroke.Key);
+                    //_logger.LogInformation(10370, "ConsoleKey was received {KeyStroke}.", keyStroke.Key);
+                    Logs.Here().Information("ConsoleKey was received {KeyStroke}.", keyStroke.Key);
                 }
 
                 await Task.Delay(10, _cancellationToken);
--- ConstantData/Program.cs
+++ ConstantData/Program.cs
@@ -1,19 +1,29 @@
 using System;
+using System.Diagnostics;
 using System.IO;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Configuration;
 using CachingFramework.Redis;
 using CachingFramework.Redis.Contracts.Providers;
 using StackExchange.Redis;
+using Microsoft.Extensions.Configuration;
 using ConstantData.Services;
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Sinks.SystemConsole.Themes;
 using Shared.Library.Services;
 
 namespace ConstantData
 {
     public class Program
     {
+        private static Serilog.ILogger Logs => Serilog.Log.ForContext<Program>();
+
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)            
             .UseContentRoot(Directory.GetCurrentDirectory())
@@ -34,7 +44,34 @@ namespace ConstantData
 
                 config.AddEnvironmentVariables();
             })
-            .ConfigureLogging((ctx, log) => { /* elided for brevity */ })
+            .ConfigureLogging((ctx, sLog) =>
+            {
+                //var seriLog = new LoggerConfiguration()
+                //    .WriteTo.Console()
+                //    .CreateLogger();
+
+                //var outputTemplate = "{Timestamp:HH:mm} [{Level:u3}] ({ThreadId}) {Message}{NewLine}{Exception}";
+                //var outputTemplate = "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message}{NewLine}in method {MemberName} at {FilePath}:{LineNumber}{NewLine}{Exception}{NewLine}";
+                string outputTemplate = "{NewLine}[{Timestamp:HH:mm:ss} {Level:u3} ({ThreadId}) {SourceContext}.{MemberName} - {LineNumber}] {NewLine} {Message} {NewLine} {Exception}";
+
+                //seriLog.Information("Hello, Serilog!");
+
+                //Log.Logger = seriLog;
+                // попробовать менять уровень вывода логгера через переменную
+                // const LogEventLevel loggerLevel = LogEventLevel.Debug;
+                // https://stackoverflow.com/questions/25477415/how-can-i-reconfigure-serilog-without-restarting-the-application
+                // https://stackoverflow.com/questions/51389550/serilog-json-config-logginglevelswitch-access
+                const LogEventLevel loggerLevel = LogEventLevel.Information;
+                Log.Logger = new LoggerConfiguration()
+                    .Enrich.With(new ThreadIdEnricher())
+                    .Enrich.FromLogContext()
+                    .MinimumLevel.Verbose()
+                    .WriteTo.Console(restrictedToMinimumLevel: loggerLevel, outputTemplate: outputTemplate, theme: AnsiConsoleTheme.Literate) //.Verbose .Debug .Information .Warning .Error .Fatal
+                    .WriteTo.File("logs/ConstantData{Date}.txt", rollingInterval: RollingInterval.Day, outputTemplate: outputTemplate)
+                    .CreateLogger();
+
+                Logs.Information("The global logger Serilog has been configured.\n");
+            })
             .UseDefaultServiceProvider((ctx, opts) => { /* elided for brevity */ })
             .ConfigureServices((hostContext, services) =>
                 {
@@ -69,5 +106,26 @@ namespace ConstantData
 
             host.Run();
         }
-    }    
+    }
+
+    internal class ThreadIdEnricher : ILogEventEnricher
+    {
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty(
+                "ThreadId", Thread.CurrentThread.ManagedThreadId));
+        }
+    }
+
+    public static class LoggerExtensions
+    {
+        // https://stackoverflow.com/questions/29470863/serilog-output-enrich-all-messages-with-methodname-from-which-log-entry-was-ca/46905798
+
+        public static ILogger Here(this ILogger logger, [CallerMemberName] string memberName = "", [CallerLineNumber] int sourceLineNumber = 0)
+            //[CallerFilePath] string sourceFilePath = "",
+        {
+            return logger.ForContext("MemberName", memberName).ForContext("LineNumber", sourceLineNumber);
+            //.ForContext("FilePath", sourceFilePath)
+        }
+    }
 }
